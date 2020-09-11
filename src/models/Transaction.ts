2fc55bd0449596d6fae7385aeb37f5b8d521b4b5
André Brandão <andrenbrandao@gmail.com>
2020-09-10 22:59:50 -0300

    Create Transaction Model

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal', {
+    precision: 10,
+    scale: 2,
+    transformer: {
+      to: (value: number): number => value,
+      from: (value: string): number => parseFloat(value),
+    },
+  })
   value: number;
 
-  category_id: string;
+  // category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,36 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total - value < 0) {
+      throw new AppError(
+        'Current balance is insufficient to make this transaction.',
+      );
+    }
+
+    const transaction = await transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
