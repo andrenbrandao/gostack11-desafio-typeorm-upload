3ac9320f686b0667cd6d944c3e6a2ca1adbf8f33
André Brandão <andrenbrandao@gmail.com>
2020-09-23 19:53:52 -0300

    Add eager loading for Transaction

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -5,7 +5,10 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   Index,
+  OneToMany,
 } from 'typeorm';
+
+import Transaction from './Transaction';
 
 @Entity('categories')
 class Category {
@@ -15,6 +18,9 @@ class Category {
   @Index({ unique: true })
   @Column()
   title: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 
   @CreateDateColumn()
   created_at: Date;
